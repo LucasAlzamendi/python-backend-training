b70a539ec3591b88f1f15f2bb8a883c84ab83870
LucasAlzamendi <113138111+LucasAlzamendi@users.noreply.github.com>
2026-01-27 15:33:17 -0300

    repaso op_aritmeticos en python

---
+++ ejercicio5_op_aritmeticos.py
@@ -0,0 +1,31 @@
+# suma y resta (+ y -)
+suma = 8 + 9
+resta = 8 - 9 
+
+print(f'Suma: {suma}')
+print(f'Resta: {resta}')
+
+# multi y div (* y /)
+multi = 8 * 9
+divi = 8 / 9 # Devuelve un float
+
+print(f'Multi: {multi}')
+print(f'Divi: {divi}')
+
+# potenciacion (exponente) (**)
+
+exponente = 8 ** 9
+
+print(f'Exponente: {exponente}')
+
+# división baja (//)
+
+division_baja = 8 // 9 # Devuelve un entero redondeado hacia abajo
+
+print(f'Division_Baja: {division_baja}')
+
+# resto o módulo (%)
+
+resto = 8 % 9
+
+print(f'Resto: {resto}')
