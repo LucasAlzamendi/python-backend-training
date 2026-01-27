cf92afc048873c0485cd2637a9a338d333502095
LucasAlzamendi <113138111+LucasAlzamendi@users.noreply.github.com>
2026-01-27 10:02:49 -0300

    modificacion ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,7 +1,8 @@
 #-- Hacer un programa que pida nombre, edad e informe "Hola [nombre], en 5 años tendras [años * 5]"
+
 nombre = input("Ingrese su nombre: ")
 edad = int(input("Ingrese su edad: "))
 futuro = int(input("Ingrese cantidad de años: "))
-e_F = edad + futuro
-texto = f"Hola {nombre}, en {futuro} años tendras {e_F}"
-print(texto)
+edad_futuro = edad + futuro
+
+print(f"Hola {nombre}, en {futuro} años tendras {edad_futuro}")
