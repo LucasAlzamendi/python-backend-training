1be3b38ed7f3435c7586c9ca10baca029104f751
LucasAlzamendi <113138111+LucasAlzamendi@users.noreply.github.com>
2026-01-28 09:01:13 -0300

    Correccion y mejoras de validaciones

--- D2-python-condicionales/ejercicio1_condicionales.py
+++ D2-python-condicionales/ejercicio1_condicionales.py
@@ -2,7 +2,7 @@
 while True:
     try:
         edad = int(input('Ingrese su edad: '))
-        if 0<= edad:
+        if 0 < edad:
             break
         else:
             print('Edad no valida.')
@@ -16,22 +16,21 @@ if 18 <= edad:
 else:
     print('No podes pasar')    
 
-# Condicional if - elif
-
 while True:
     try:
         nueva_edad = int(input('Ingrese una nueva edad: '))
-        if 0 <= edad:
+        if 0 < nueva_edad:
             break
         else:
             print('Edad no valida.')
     except ValueError:
         print('Ingrese una edad en numeros.')   
 
+# Condicional if - elif
+
 if 18 <= nueva_edad:
     print('No hay problema. Podes pasar.')
 elif 14 <= nueva_edad < 18:
     print('Hay un problema. No te da la edad. No podes pasar.')
-elif 0< nueva_edad < 14:
+else:
     print('Pibe, ¿Qué haces aca? ¿Dónde estan tus papas?')
-
