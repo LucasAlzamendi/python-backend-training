0b61441989363185f2357a0b00616573f8147044
LucasAlzamendi <113138111+LucasAlzamendi@users.noreply.github.com>
2026-01-28 08:18:31 -0300

    Reorganizacion estructura: mover Dia 1 a carpeta D1-python

---
+++ D1-python/ejercicio6_condicionales.py
@@ -0,0 +1,37 @@
+# Repasando condicionales en python
+while True:
+    try:
+        edad = int(input('Ingrese su edad: '))
+        if 0<= edad:
+            break
+        else:
+            print('Edad no valida.')
+    except ValueError:
+        print('Ingrese su edad en numeros.')    
+
+# Condicional if - else
+
+if 18 <= edad:
+    print('Podes pasar')
+else:
+    print('No podes pasar')    
+
+# Condicional if - elif
+
+while True:
+    try:
+        nueva_edad = int(input('Ingrese una nueva edad: '))
+        if 0 <= edad:
+            break
+        else:
+            print('Edad no valida.')
+    except ValueError:
+        print('Ingrese una edad en numeros.')   
+
+if 18 <= nueva_edad:
+    print('No hay problema. Podes pasar.')
+elif 14 <= nueva_edad < 18:
+    print('Hay un problema. No te da la edad. No podes pasar.')
+elif 0< nueva_edad < 14:
+    print('Pibe, ¿Qué haces aca? ¿Dónde estan tus papas?')
+
