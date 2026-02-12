3e0d0fcaa998dfe93a2ba5fe22b5616ca94a201a
LucasAlzamendi <113138111+LucasAlzamendi@users.noreply.github.com>
2026-02-12 09:59:30 -0300

    Condicionales y sistema de login

---
+++ D2-python/sistema_login.py
@@ -0,0 +1,26 @@
+# Ejercicio ChatGPT - Crear un pequeño menu de login en el cual se le pida al usuario que ingrese: "nombre_de_usuario" y 
+# "contraseña_usuario" y si son iguales a: "nombre_usario_guardado" y "contraseña_usuario_guardada"
+
+# Datos del usuario
+
+id = "admin"
+
+password = "1234"
+
+attemps = 3
+
+acceso = False
+# Ingreso y verificación de los datos del usuario
+
+while 0 < attemps and not acceso:
+    user_id = input("Ingrese su nombre de usuario: ")
+    user_password = input("Ingrese su contraseña: ")
+    if user_id == id and user_password == password:
+        print("Login exitoso")
+        acceso = True
+    else:
+        attemps -= 1
+        print(f"Credenciales incorrectas. Intetnos restantes {attemps}")
+
+if not acceso:
+    print("Acceso denegado. Cuenta bloqueada")
