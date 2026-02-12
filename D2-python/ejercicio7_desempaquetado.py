a52e56897c4e61fde817645d6654e4827d636a27
LucasAlzamendi <113138111+LucasAlzamendi@users.noreply.github.com>
2026-02-12 10:04:43 -0300

    Aprendiendo a desempaquetar datos de tuplas

---
+++ D2-python/ejercicio7_desempaquetado.py
@@ -0,0 +1,11 @@
+# Creaion de datos
+
+datos = ("Lucas", "Alzamedi", 24)
+
+# Desempaquetado 
+
+nombre , apellido , edad = datos
+
+# Mostrando resultados
+
+print(f"La persona se llama {nombre} {apellido} y tiene {edad} años")
