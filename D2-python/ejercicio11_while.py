3d1e3a99ea2300cdc30fd6251ec75f99cccb1c2e
LucasAlzamendi <113138111+LucasAlzamendi@users.noreply.github.com>
2026-02-12 10:14:32 -0300

    Introduciendo bucle While

---
+++ D2-python/ejercicio11_while.py
@@ -0,0 +1,6 @@
+# WHILE - Mientras la condicion sea TRUE se sigue ejecutando, tras cada loop se verifica la condicion y si es FALSE termina
+# el WHILE
+num = 0
+while num <= 16 :
+    print(f"{num}")
+    num += 1 
