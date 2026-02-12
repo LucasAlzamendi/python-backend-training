ec51da3430218ed7842973fe4a943b329ff094db
LucasAlzamendi <113138111+LucasAlzamendi@users.noreply.github.com>
2026-02-12 10:01:02 -0300

    Aprendiendo a definir Tuplas

---
+++ D1-python/ejercicio6_tuplas.py
@@ -0,0 +1,18 @@
+# Diferentes formas de crear y trabajar con Tuplas
+# NOTA : a la hora de trabajar con datos que NO SE DEBEN MODIFICAR se recomienda el uso de tuplas 
+
+# Convirtiendo una lista a tupla con tuple()
+
+tupla_01 = tuple(["dato_01", "dato_02"])
+
+# Creando tupla sin parentesis
+
+tupla_02 = "dato_03", "dato_04"
+
+# Creando tupla sin parentesis de un solo elemento
+
+tupla_03 = "dato_05",
+
+print(tupla_01)
+print(tupla_02)
+print(tupla_03)
